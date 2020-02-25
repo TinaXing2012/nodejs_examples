c335e0e5f17810c436a6b29671c2aeff17809905
rxing <rxing@mum.edu>
2020-02-24 21:26:21 -0600

    path.join demo

--- day2/fs/app.js
+++ day2/fs/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const names = fs.readFileSync('./names.txt', 'utf8');
 console.log(names);
@@ -14,4 +15,9 @@ fs.writeFileSync('./greeting1.txt', 'Hello from TIna', 'utf8');
 fs.writeFile('./greeting2.txt', 'Hello from TIna', 'utf8', function (err) {
     if (err) throw err;
     console.log('DOne');
+});
+
+fs.readFile(path.join(__dirname, './a/b/hello.txt'), { encoding: 'utf8' }, function (err, data) {
+    if (err) console.log('err.......');
+    console.log(data, '======');
 });
