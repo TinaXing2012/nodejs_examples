9791dad2951c1284c89bb2d8db243ed93657d89c
rxing <rxing@mum.edu>
2020-02-24 19:55:56 -0600

    resolve demos

---
+++ day2/app.js
@@ -0,0 +1,8 @@
+const add = require('mymath');
+console.log(add(2, 3));
+
+const substract = require('./math/main');
+console.log(substract(3, 1));
+
+const divide = require('./somefolder');
+console.log(divide(8, 2));
---
+++ day2/math/main.js
@@ -0,0 +1,3 @@
+module.exports = function substract(a, b) {
+    return a - b;
+}
