1581d31984f15af2192c9f91039fcc469e658078
rxing <rxing@mum.edu>
2020-02-26 21:40:38 -0600

    express - configuration

--- day4/express-demo/app.js
+++ day4/express-demo/app.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.enable('case sensitive routing');
+app.enable('etag');
+app.set('env', 'development');
+
+app.use('/users', (req, res, next) => {
+    res.send('Hello from /users');
+});
+//  '/' => '/*'
+app.use('/', (req, res, next) => {
+    res.send('Hello from Express!!!');
+});
+
+
 app.listen(3000, function () {
     console.log('server is running on 3000');
 });
