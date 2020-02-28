571cdf5f647e92bbfcf125f652c4dd8ec293d39e
rxing <rxing@mum.edu>
2020-02-27 21:14:10 -0600

    app.verb()

--- day5/app.js
+++ day5/app.js
@@ -8,17 +8,22 @@ const app = express();
 //true: qs
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
+app.get('/add-product', (req, res, next) => {
     // res.setHeader('content-type', 'text/html');
     // const file = fs.readFileSync(path.join(__dirname, 'views', 'add-product.html'), 'utf-8');
     // res.send(file.toString());
     fs.createReadStream(path.join(__dirname, 'views', 'add-product.html')).pipe(res);
 });
 
-app.use('/save-product', (req, res, next) => {
+app.post('/add-product', (req, res, next) => {
     console.log('save product...');
     console.log(req.body);
     res.send('Save Successfully');
 });
+
+app.all('/', (req, res, next) => {
+    res.send('This is / page');
+});
+
 
 app.listen(3000);
