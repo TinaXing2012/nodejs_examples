b3e7cac8944c0ae92cf5deb250e38d78ae588779
rxing <rxing@mum.edu>
2020-02-27 21:47:57 -0600

    error handling

--- day5/app.js
+++ day5/app.js
@@ -18,4 +18,8 @@ app.use((req, res, next) => {
     res.status(404).send('Page Not Found');
 });
 
+app.use((err, req, res, next) => {
+    res.status(500).send('Something Broke!');
+});
+
 app.listen(3000);
--- day5/routes/shop.js
+++ day5/routes/shop.js
@@ -6,4 +6,8 @@ router.all('/', (req, res, next) => {
     res.send('This is / page');
 });
 
+router.get('/error-demo', (req, res, next) => {
+    throw new Error('This is to test Error handling in express');
+});
+
 module.exports = router;
