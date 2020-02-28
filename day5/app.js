a3563844ae606775fb58e5648baa4544501de6f1
rxing <rxing@mum.edu>
2020-02-27 21:38:28 -0600

    404

--- day5/app.js
+++ day5/app.js
@@ -14,4 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('Page Not Found');
+});
+
 app.listen(3000);
