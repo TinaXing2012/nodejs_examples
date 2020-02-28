b6f21c749a865976d71f6793dbfac988443d50d8
rxing <rxing@mum.edu>
2020-02-27 22:09:26 -0600

    static resources

--- day5/app.js
+++ day5/app.js
@@ -10,6 +10,14 @@ const app = express();
 //false: querystring
 //true: qs
 app.use(bodyParser.urlencoded({ extended: true }));
+//http://localhost:3000/css/main.css
+// app.use(express.static(path.join(__dirname, 'public')));
+//http://localhost:3000/abc => package till public
+// http://localhost:3000/abc/css/main.css 
+// app.use('/abc', express.static(path.join(__dirname, 'public')));
+//http://localhost:3000/abc => css folder
+//http://localhost:3000/abc/main.css
+app.use('/abc', express.static(path.join(__dirname, 'public', 'css')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
