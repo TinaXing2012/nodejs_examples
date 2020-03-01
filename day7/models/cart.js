3410f59a552f8f6cfd055856e7beec84175ca584
rxing <rxing@mum.edu>
2020-03-01 15:46:09 -0600

    Refactor add to shopping cart

--- day7/models/cart.js
+++ day7/models/cart.js
@@ -4,25 +4,21 @@ module.exports = class Cart {
 
     static save(product) {
 
-        if (cart) { // cart is not null
-            const existingProductIndex = cart.products.findIndex(p => p.id == product.id); // to check product is existing in cart
-            console.log('existingProductIndex: ', existingProductIndex);
-            if (existingProductIndex >= 0) { // exist in cart already
-                const exsitingProduct = cart.products[existingProductIndex];
-                exsitingProduct.qty += 1;
-                cart.totalPrice += product.price;
-
-            } else { //not exist
-                product.qty = 1;
-                cart.products.push(product);
-                cart.totalPrice += product.price;
-            }
-        } else {
+        if (cart === null) {
             cart = { products: [], totalPrice: 0 };
+        }
+
+        const existingProductIndex = cart.products.findIndex(p => p.id == product.id); // to check product is existing in cart
+        console.log('existingProductIndex: ', existingProductIndex);
+        if (existingProductIndex >= 0) { // exist in cart already
+            const exsitingProduct = cart.products[existingProductIndex];
+            exsitingProduct.qty += 1;
+        } else { //not exist
             product.qty = 1;
             cart.products.push(product);
-            cart.totalPrice = product.price;
         }
+
+        cart.totalPrice += product.price;
     }
 
     static getCart() {
