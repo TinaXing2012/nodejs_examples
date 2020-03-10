ade8a49b4e309f90dca7a84b8a4f477da150e7b2
rxing <rxing@mum.edu>
2020-03-09 20:34:35 -0500

    user model

---
+++ day10/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [{
+            productId: {
+                type: mongoose.SchemaTypes.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            qty: {
+                type: Number,
+                required: true
+            }
+        }],
+        totalPrice: Number
+    }
+});
+//'User' => users
+module.exports = mongoose.model('User', userSchema);
