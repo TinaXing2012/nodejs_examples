1b9aa2b031d8655425e7fc20c0b85c209837eab0
rxing <rxing@mum.edu>
2020-03-21 14:44:28 -0500

    pattern 4

---
+++ day2/module_patterns/pattern4/app.js
@@ -0,0 +1,8 @@
+const Person = require('./pattern4');
+const p1 = new Person();
+p1.name = 'Tina Xing...';
+console.log(p1.name);
+
+const Person2 = require('./pattern4');
+const p2 = new Person2();
+console.log(p2.name);
---
+++ day2/module_patterns/pattern4/pattern4.js
@@ -0,0 +1,8 @@
+module.exports = class Person {
+    constructor(name = 'Miss Xing') {
+        this.name = name;
+    }
+
+}
+
+// module.exports = Person;
